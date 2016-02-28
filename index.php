e924de64ce74ca4e7f756d85129342e113f3d4ba
bennyg95 <sanchezbenitog95@hotmail.com>
2016-02-27 19:04:56 -0800

    Adding the displaywinners() function. this will display the winners in the game

--- index.php
+++ index.php
@@ -4,65 +4,65 @@ function main() {
     $deck = array();
     $scores = array();
     for($i = 1; $i <= 52; $i++) {
-        $deck[] = $i;
+	$deck[] = $i;
     }
     shuffle($deck);
     echo "<table size='50%'>";
-        echo "<tr>";
-        
-            $temp = getHand($deck, $scores); 
-            echo "<td>"; 
-                echo "<img src='silverJack/img/faces/ricardo.png' width='20%' alt='Ricky'/>";
-                displayHand($temp);
-            echo "</td>";
-            echo "<td>";
-                playersPoints($scores, 0);
-            echo "</td>";
-            
-        echo "</tr>";
-        
-        echo "<tr>";
-        
-            $fallas = getHand($deck, $scores);
-            echo "<td>";    
-                echo "<img src='silverJack/img/faces/benito.jpg' width='20%' alt='Benito'/>";
-                displayHand($fallas);
-            echo "</td>";
-            echo "<td>";
-                playersPoints($scores, 1);
-            echo "</td>";
-            
-        echo "</tr>";
-        
-        echo "<tr>";
-            
-            $temp = getHand($deck, $scores); 
-            echo"<td>";
-                echo "<img src='silverJack/img/faces/stephen.jpg' width='20%' alt='Stephen' />";
-                displayHand($temp);
-            echo "</td>";
-            echo "<td>";
-           
-                playersPoints($scores, 2);
-            
-            echo "</td>";
-        
-        echo "</tr>";
-        
-        echo "<tr>";
-            
-            $temp = getHand($deck, $scores); 
-            echo "<td>";
-                echo "<img src='silverJack/img/faces/clarissa.jpg' width='20%' alt='Clarissa'/>";
-                displayHand($temp);
-            echo "</td>"; 
-            echo "<td>";
-                playersPoints($scores, 3);
-            echo "</td>";
-        echo "</tr>";
-            
-    echo "</table>"; 
-    
+	echo "<tr>";
+
+	    $temp = getHand($deck, $scores);
+	    echo "<td>";
+		echo "<img src='silverJack/img/faces/ricardo.png' width='20%' alt='Ricky'/>";
+		displayHand($temp);
+	    echo "</td>";
+	    echo "<td>";
+		playersPoints($scores, 0);
+	    echo "</td>";
+
+	echo "</tr>";
+
+	echo "<tr>";
+
+	    $fallas = getHand($deck, $scores);
+	    echo "<td>";
+		echo "<img src='silverJack/img/faces/benito.jpg' width='20%' alt='Benito'/>";
+		displayHand($fallas);
+	    echo "</td>";
+	    echo "<td>";
+		playersPoints($scores, 1);
+	    echo "</td>";
+
+	echo "</tr>";
+
+	echo "<tr>";
+
+	    $temp = getHand($deck, $scores);
+	    echo"<td>";
+		echo "<img src='silverJack/img/faces/stephen.jpg' width='20%' alt='Stephen' />";
+		displayHand($temp);
+	    echo "</td>";
+	    echo "<td>";
+
+		playersPoints($scores, 2);
+
+	    echo "</td>";
+
+	echo "</tr>";
+
+	echo "<tr>";
+
+	    $temp = getHand($deck, $scores);
+	    echo "<td>";
+		echo "<img src='silverJack/img/faces/clarissa.jpg' width='20%' alt='Clarissa'/>";
+		displayHand($temp);
+	    echo "</td>";
+	    echo "<td>";
+		playersPoints($scores, 3);
+	    echo "</td>";
+	echo "</tr>";
+
+    echo "</table>";
+
     echo "<br/>";
     echo "<div id='winners'>";
     displayWinners($scores);
@@ -76,41 +76,77 @@ function getHand(&$deck, &$scores) {
     unset($hand);
     $hand = array();
     while(! $done) {
-        $choice = rand(0, 1);
-        if($sum < 36) {
-            $card = array_pop($deck);
-            if($card == 52)
-                $card--;
-            $cardValue = $card % 13;
-            if($cardValue == 0)
-                $cardValue = 13;
-    
-            array_push($hand, $card);
-            $sum = $sum + $cardValue;
-        }
-        else if($sum < 42 && $choice == 1) {
-            $card = array_pop($deck);
-            if($card == 52)
-                $card--;
-            $cardValue = $card % 13;
-            if($cardValue == 0)
-            {
-                $cardValue = 13;
-            }
-    
-            array_push($hand, $card);
-            $sum = $sum + $cardValue;
-            
-        }
-          
-            
-        else {
-            array_push($scores, $sum);  
-            $done = true;
-        }
+	$choice = rand(0, 1);
+	if($sum < 36) {
+	    $card = array_pop($deck);
+	    if($card == 52)
+		$card--;
+	    $cardValue = $card % 13;
+	    if($cardValue == 0)
+		$cardValue = 13;
+
+	    array_push($hand, $card);
+	    $sum = $sum + $cardValue;
+	}
+	else if($sum < 42 && $choice == 1) {
+	    $card = array_pop($deck);
+	    if($card == 52)
+		$card--;
+	    $cardValue = $card % 13;
+	    if($cardValue == 0)
+	    {
+		$cardValue = 13;
+	    }
+
+	    array_push($hand, $card);
+	    $sum = $sum + $cardValue;
+
+	}
+
+
+	else {
+	    array_push($scores, $sum);
+	    $done = true;
+	}
     }
-    
+
     return($hand);
+}
+
+function displayWinners(&$scores) {
+    $winners = array();
+
+    $greatestIndex = 0;
+    if($scores[$greatestIndex] > 42) {
+	do{
+	    $greatestIndex++;
+	}while(($scores[$greatestIndex] > 42) && ($greatestIndex < sizeof($scores)));
+    }
+    // 41 47 37 44
+    for($currIndex = $greatestIndex; $currIndex < sizeof($scores); $currIndex++) {
+	if($scores[$currIndex] <= 42) {
+	    if($scores[$currIndex] > $scores[$greatestIndex]) {
+		unset($winners);
+		$winners = array();
+		$winners[] = $currIndex;
+		$greatestIndex = $currIndex;
+	    }
+	    else if($scores[$currIndex] == $scores[$greatestIndex]){
+		$winners[] = $currIndex;
+
+	    }
+	}
+    }
+
+    $names = array("Ricky", "Benito", "Stephen", "Clarissa");
+
+    if(sizeof($winners) == 1) {
+	$points = array_sum($scores) - $scores[$winners[0]];
+	echo $names[$winners[0]] . " Wins " . $points . " points!";
+    }
+    else {
+	echo "TIE!";
+    }
 }
 
 ?>
